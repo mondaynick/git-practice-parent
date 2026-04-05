0865a861b7215a6e539baded5c973856199aaf60
mdn190 <zy0932135677@gmail.com>
2026-04-05 12:53:07 +0800

    Add git-practice-sub as submodule and call add function

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "git-practice-sub/auxi.h"
+
+using namespace std;
+
+int main() {
+    int x = 10;
+    int y = 3;
+    cout << "add(" << x << "," << y << ") = " << add(x, y) << endl;
+    return 0;
+}
