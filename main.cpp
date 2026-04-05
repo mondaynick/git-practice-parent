d6658f01a8500f4738692521aec59a673d7654ea
mdn190 <zy0932135677@gmail.com>
2026-04-05 13:02:45 +0800

    Update submodule and test subtract function

--- main.cpp
+++ main.cpp
@@ -7,5 +7,6 @@ int main() {
     int x = 10;
     int y = 3;
     cout << "add(" << x << "," << y << ") = " << add(x, y) << endl;
+    cout << "sub(" << x << "," << y << ") = " << sub(x, y) << endl;
     return 0;
 }
